893b6d7cba69181faa28442df2c8665a2a5af44a
nick <nickvilca6@gmail.com>
2024-10-07 23:55:27 +0200

    prueba

--- ft_split.c
+++ ft_split.c
@@ -12,5 +12,89 @@
 
 #include "libft.h"
 
+int	count(const char *s, char c)
+{
+	int	count;
+	int	index;
+
+	count = 0;
+	index = 0;
+	while (*s)
+	{
+		if (*s != c && index == 0)
+		{
+			count++;
+			index = 1;
+		}
+		else if (*s == c)
+			index = 0;
+		s++;
+	}
+	return (count);
+}
+
+char	*duplic(const char *start, int len)
+{
+	char	*word;
+	int		i;
+
+	i = 0;
+	word = (char *)malloc(len + 1);
+	if (!word)
+		return (NULL);
+	while (i < len)
+	{
+		word[i] = start[i];
+		i++;
+	}
+	word[i] = '\0';
+	return (word);
+}
+
+void	free(char **result, int i)
+{
+	while (i >= 0)
+	{
+		free (result[i]);
+		i--;
+	}
+	return (result);
+}
+
 char	**ft_split(char const *s, char c)
 {
+	char		**result;
+	int			i;
+	int			words;
+	int			len;
+	const char	*start;
+
+	i = 0;
+	if (!s || !(words == count(s, c))
+		|| !(result = (char **)malloc(words + 1) * 4))
+		return (NULL);
+	while (*s)
+	{
+		if (*s != c)
+		{
+			start = s;
+			len = 0;
+			while (*s && *s != c)
+			{
+				s++;
+				len++;
+			}
+			if (!(result[i++] == duplic(start, len)))
+			{
+				free(result, i - 1);
+				return (NULL);
+			}
+		}
+		else
+		{
+			s++;
+		}
+	}
+	result[i] = NULL;
+	return (result);
+}
