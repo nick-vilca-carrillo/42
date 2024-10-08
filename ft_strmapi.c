6f6ecbb9225295041c30258d1ae41867c0bbb75a
nick vilca carrillo <161527120+nick-vilca-carrillo@users.noreply.github.com>
2024-10-08 10:46:35 +0200

    Update ft_strmapi.c

--- ft_strmapi.c
+++ ft_strmapi.c
@@ -9,5 +9,25 @@
 /*   Updated: 2024/10/05 14:29:31 by nivilca-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+#include "libft.h"
 
+char  *ft_strmapi(const char *s, char(*f)(unsigned int, char))
+{
+  unsigned int  i;
+  size_t        len;
+  char          *new_str;
 
+  i = 0;
+  len = ft_strlen(s);
+  new_str = (char *)malloc(len + 1);
+  if (!new_str)
+    return (NULL);
+  while (i < len)
+  {
+      new_str[i] = f(i, s[i]);
+      i++;
+  }
+  new_str = '\0';
+  return new_str;
+}
+  
