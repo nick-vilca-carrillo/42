9094b1d4cda45b596f9415d91890ca7a7b48f686
nick vilca carrillo <161527120+nick-vilca-carrillo@users.noreply.github.com>
2024-10-09 22:26:12 +0200

    Update ft_putnbr_fd.c

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -10,4 +10,19 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+void ft_putnbr_fd(int n, int fd) {
+    if (n == 0) {
+        write(fd, "0", 1);
+        return;
+    }
+    if (n < 0) {
+        write(fd, "-", 1); 
+        n = -n; 
+    }
+    if (n >= 10) {
+        ft_putnbr_fd(n / 10, fd); 
+    }
+    char digit = (n % 10) + '0';
+    write(fd, &digit, 1);
+}
 
