7e87afcad44824f9e01496cae18d73dda6f83738
nick vilca carrillo <161527120+nick-vilca-carrillo@users.noreply.github.com>
2024-10-09 22:11:24 +0200

    Update ft_striteri.c

--- ft_striteri.c
+++ ft_striteri.c
@@ -12,7 +12,13 @@
 
 #include "libft.h"
 
-void  ft_striteri(char *s,void (*f)(unsigned int, char*))
-{
-  
+void ft_striteri(char *s, void (*f)(unsigned int, char*)) {
+    unsigned int i;
+
+    i = 0
+    while (s[i] != '\0') {
+        f(i, &s[i]); 
+        i++; 
+    }
+}
 
